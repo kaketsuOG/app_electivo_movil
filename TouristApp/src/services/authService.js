9c53c7ec2f68a3ba31d3dd7038ba091b3577d6d6
GeoC <diego.cubilhos@gmail.com>
2024-11-14 18:37:22 -0300

    Actualizacion de version

--- TouristApp/src/services/authService.js
+++ TouristApp/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.0.2:3000/auth';
+const API_URL = 'http://192.168.1.82:3000/auth';
 
 const register = async (username, email, password) => {
     const response = await axios.post(`${API_URL}/signup`, { username, email, password });
